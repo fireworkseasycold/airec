4b772e8bf73c287b20dec2200f67a601ce5719f2
fireworkseasycold <1476094297@qq.com>
2022-08-22 13:38:21 +0800

    'updatesenbase64demo'

--- demo/sendbase64demo.py
+++ demo/sendbase64demo.py
@@ -20,23 +20,28 @@ import numpy as np
 import requests
 
 
-url='http://127.0.0.1:8000/face/receivecamera'
-image1 = cv2.imread("testimg/who.jpg")
-aa = base64.b64encode(cv2.imencode('.jpg', image1)[1]).decode()
-# print(aa)
-#我这里接口实际上接收的约定数据是{picture:"data:image/jpeg;base64,xxx"}
-print('开始模拟post')
-response = requests.post(url, data={"picture":'data:image/jpeg;base64,'+aa})
-print(response.content.decode("utf-8"))  #因为我后端有get方法渲染页面，这里也会带上页面
-print(response)
+# url='http://127.0.0.1:8000/face/receivecamera'
+# image1 = cv2.imread("testimg/who.jpg")
+# aa = base64.b64encode(cv2.imencode('.jpg', image1)[1]).decode()
+# # print(aa)
+# #我这里接口实际上接收的约定数据是{picture:"data:image/jpeg;base64,xxx"}
+# print('开始模拟post')
+# response = requests.post(url, data={"picture":'data:image/jpeg;base64,'+aa})
+# print(response.content.decode("utf-8"))  #因为我后端有get方法渲染页面，这里也会带上页面
+# print(response.text)
+# print(response.content)
+# print(response)
+# print("请求头:%s" %r.request.headers)
+# print("响应头:%s" %r.headers)
 
-
-# 服务端：
-def image_base64(request):
-    result = request.POST.get("picture")
-    img_byte = base64.b64decode(result)
-    img_np_arr = np.fromstring(img_byte, np.uint8)
-    image = cv2.imdecode(img_np_arr, cv2.IMREAD_COLOR)
+#
+#
+# # 服务端：
+# def image_base64(request):
+#     result = request.POST.get("picture")
+#     img_byte = base64.b64decode(result)
+#     img_np_arr = np.fromstring(img_byte, np.uint8)
+#     image = cv2.imdecode(img_np_arr, cv2.IMREAD_COLOR)
 
 
 
@@ -46,9 +51,11 @@ def image_base64(request):
 url2='http://127.0.0.1:8000/face/receivecamerajson'
 image1 = cv2.imread("testimg/who.jpg")
 bb= base64.b64encode(cv2.imencode('.jpg', image1)[1]).decode()
-r = requests.post(url2, json={"picture": bb})
-print(r.content.decode("utf-8"))
+r = requests.post(url2, json={"picture": 'data:image/jpeg;base64,'+bb})
 
+print(r.content.decode("utf-8"))
+print("请求头:%s" %r.request.headers)
+print("响应头:%s" %r.headers)
 
 
 # 服务端：
